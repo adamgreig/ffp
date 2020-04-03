4c61072073cbc0df47b941dfbe64956b5148acbf
Adam Greig <adam@adamgreig.com>
2020-04-03 12:02:37 +0100

    update main to still run usb

--- firmware/src/main.rs
+++ firmware/src/main.rs
@@ -118,49 +118,8 @@ pub fn swd_demo(swd: &swd::SWD) -> Result<[u32; 4]> {
     Ok(mem)
 }
 
-#[entry]
-fn main() -> ! {
-    let flash = hal::flash::Flash::new(stm32ral::flash::Flash::take().unwrap());
-    let rcc = hal::rcc::RCC::new(stm32ral::rcc::RCC::take().unwrap(),
-                                 stm32ral::crs::CRS::take().unwrap());
-    //let nvic = hal::nvic::NVIC::new(stm32ral::nvic::NVIC::take().unwrap(),
-                                    //stm32ral::scb::SCB::take().unwrap());
-    let gpioa = hal::gpio::GPIO::new(stm32ral::gpio::GPIOA::take().unwrap());
-    let gpiob = hal::gpio::GPIO::new(stm32ral::gpio::GPIOB::take().unwrap());
-    let spi = hal::spi::SPI::new(stm32ral::spi::SPI1::take().unwrap());
-
-    // Define pinout
-    let sck = gpioa.pin(5);
-    let flash_si = gpioa.pin(7);
-    let flash_si_input_mode = flash_si.memoise_mode_input();
-    let flash_si_alternate_mode = flash_si.memoise_mode_alternate();
-    let sck_output_mode = sck.memoise_mode_output();
-    let sck_alternate_mode = sck.memoise_mode_alternate();
-    let pins = hal::gpio::Pins {
-        led: gpioa.pin(2),
-        cs: gpioa.pin(3),
-        fpga_rst: gpioa.pin(4),
-        sck: gpioa.pin(5),
-        flash_so: gpioa.pin(6),
-        flash_si,
-        fpga_so: gpiob.pin(4),
-        fpga_si: gpiob.pin(5),
-        tpwr_det: gpiob.pin(6),
-        tpwr_en: gpiob.pin(7),
-
-        flash_si_input_mode,
-        flash_si_alternate_mode,
-        sck_output_mode,
-        sck_alternate_mode,
-    };
+pub fn swd_main(spi: &hal::spi::SPI, pins: &hal::gpio::Pins) -> ! {
     let swd = swd::SWD::new(&spi, &pins);
-
-    flash.setup();
-    rcc.setup();
-    pins.setup();
-    pins.swd_mode();
-    pins.swd_tx();
-    spi.setup_swd();
 
     loop {
 
@@ -186,6 +145,67 @@ fn main() -> ! {
             Err(e) => {
                 hprintln!("Demo error: {:?}", e).ok();
             },
+        }
+    }
+}
+
+#[entry]
+fn main() -> ! {
+    let flash = hal::flash::Flash::new(stm32ral::flash::Flash::take().unwrap());
+    let rcc = hal::rcc::RCC::new(stm32ral::rcc::RCC::take().unwrap(),
+                                 stm32ral::crs::CRS::take().unwrap());
+    let nvic = hal::nvic::NVIC::new(stm32ral::nvic::NVIC::take().unwrap(),
+                                    stm32ral::scb::SCB::take().unwrap());
+    let dma = hal::dma::DMA::new(stm32ral::dma1::DMA1::take().unwrap());
+    let gpioa = hal::gpio::GPIO::new(stm32ral::gpio::GPIOA::take().unwrap());
+    let gpiob = hal::gpio::GPIO::new(stm32ral::gpio::GPIOB::take().unwrap());
+    let mut spi = hal::spi::SPI::new(stm32ral::spi::SPI1::take().unwrap());
+    let mut usb = hal::usb::USB::new(stm32ral::usb::USB::take().unwrap());
+
+    // Define pinout
+    let sck = gpioa.pin(5);
+    let flash_si = gpioa.pin(7);
+    let flash_si_input_mode = flash_si.memoise_mode_input();
+    let flash_si_alternate_mode = flash_si.memoise_mode_alternate();
+    let sck_output_mode = sck.memoise_mode_output();
+    let sck_alternate_mode = sck.memoise_mode_alternate();
+    let pins = hal::gpio::Pins {
+        led: gpioa.pin(2),
+        cs: gpioa.pin(3),
+        fpga_rst: gpioa.pin(4),
+        sck,
+        flash_so: gpioa.pin(6),
+        flash_si,
+        fpga_so: gpiob.pin(4),
+        fpga_si: gpiob.pin(5),
+        tpwr_det: gpiob.pin(6),
+        tpwr_en: gpiob.pin(7),
+
+        flash_si_input_mode,
+        flash_si_alternate_mode,
+        sck_output_mode,
+        sck_alternate_mode,
+    };
+
+    const SWD_DEMO: bool = false;
+    if SWD_DEMO {
+        flash.setup();
+        rcc.setup();
+        pins.setup();
+        pins.swd_mode();
+        pins.swd_tx();
+        spi.setup_swd();
+        swd_main(&spi, &pins);
+    } else {
+        // Create App instance with the HAL instances
+        let mut app = app::App::new(&flash, &rcc, &nvic, &dma, &pins, &mut spi, &mut usb);
+
+        // Initialise application, including system peripherals
+        app.setup();
+
+        loop {
+            // Process events
+            app.poll();
         }
     }
 }
